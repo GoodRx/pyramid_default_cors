28f145124677fb5e04eeda587cfd575b9de702f9
mark floyd <markfloyd@marks-MacBook-Pro.local>
2017-11-10 11:47:46 -0800

    bumping version number

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='pyramid_default_cors',
-    version='0.2',
+    version='0.2.1',
     description='Default wiring for CORS XHR',
     long_description=README,
     author='Mark Floyd',
     author_email='mark@goodrx.com',
-    url='https://github.com/emfloyd2/pyramid_default_cors',
+    url='https://github.com/GoodRx/pyramid_default_cors',
     keywords='config web wsgi pyramid',
     packages=find_packages(),
     zip_safe=False,

709c51e88d0fbe0d4115a2e88be1e7f91c59b2a8
mark floyd <markfloyd@marks-MacBook-Pro.local>
2017-11-10 11:38:22 -0800

    bumping version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='pyramid_default_cors',
-    version='0.1',
+    version='0.2',
     description='Default wiring for CORS XHR',
     long_description=README,
     author='Mark Floyd',
